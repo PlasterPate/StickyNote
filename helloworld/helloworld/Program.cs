79bd36611ab7d52542f5473826d857609b42c5d2
AliMohammad Movahedian <am.movahedian1998@hgmail.com>
2018-02-24 23:55:49 +0330

    add test units

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace helloworld
 {
-    class Program
+    public class Program
     {
+        public static bool check(int[] numbers, bool asc)
+        {
+            int first_num = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (asc)
+                    if (num < first_num)
+                        return false;
+                    else
+                        first_num = num;
+                else
+                    if (num > first_num)
+                    return false;
+                else
+                    first_num = num;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            
+
+            //Console.ReadKey();
         }
     }
 }
